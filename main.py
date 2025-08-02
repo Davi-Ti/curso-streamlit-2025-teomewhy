eef97dab45cc537f45af40070be85b9c643e1f83
Davi-Ti <davi.connect@outlook.com>
2025-08-02 16:20:33 -0300

    Fix typos and improve comments in file upload and data display sections

--- main.py
+++ main.py
@@ -7,34 +7,36 @@ st.markdown(
     """
 # Boas Vindas!
 ## Nosso APP Financeiro!
-Espero que voce curta a experiencia da nossa solução para organização financeira
+Espero que você curta a experiência da nossa solução para organização financeira
 """
 )
 
-# Widgt de upload de dadso
+# Widget de upload de arquivo
 file_upload = st.file_uploader(label="Faça upload dos dados aqui", type=["csv"])
 
-# Verifica se algum arquivo foi feito upload
+# Verifica se o arquivo foi carregado
 if file_upload:
-    # Leitura dos dados
-    df = pd.read_csv(file_upload)
-    df["Data"] = pd.to_datetime(df["Data"]).dt.date
 
-    # Exibição dos dados no App
+    # Leitura do Dados
+    df = pd.read_csv(file_upload)
+    df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y").dt.date
+
+    # Exibir os dados
     exp1 = st.expander("Dados Brutos")
-    columns_fmt = {
-        "Valor": st.column_config.NumberColumn("Valor", format="R$ %.2f"),
-        "Data": st.column_config.DateColumn("Data", format="DD/MM/YYYY"),
-    }
+    columns_fmt = {"Valor": st.column_config.NumberColumn("Valor", format="R$ %.2f")}
     exp1.dataframe(df, hide_index=True, column_config=columns_fmt)
 
-    # Visão Instituições
+    # Visao Instituições
     exp2 = st.expander("Instituições")
-    df_instituicao = df.pivot_table(index="Data", columns="Instituição", values="Valor")
-    # Cria a formatação para todas as colunas com o mesmo padrão
+    df_instituicao = df.pivot_table(
+        index="Data", columns="Instituição", values="Valor", aggfunc="sum"
+    )
+    df_instituicao = df_instituicao.sort_index()
+
     columns_fmt2 = {
         col: st.column_config.NumberColumn(label=col, format="R$ %.2f")
         for col in df_instituicao.columns
     }
-    columns_fmt2["Data"] = st.column_config.DateColumn("Data", format="DD/MM/YYYY")
+
     exp2.dataframe(df_instituicao, column_config=columns_fmt2)
+    exp2.line_chart(df_instituicao)
