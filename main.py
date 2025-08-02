b09aecb4e07ae8eac4bda6caf33f8258d7e31834
Davi-Ti <davi.connect@outlook.com>
2025-08-02 16:00:21 -0300

    Refactor data processing and enhance dataframe display with date formatting

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ file_upload = st.file_uploader(label="Faça upload dos dados aqui", type=["csv",
 # Verifica se algum arquivo foi feito upload
 if file_upload:
     df = pd.read_csv(file_upload)
+    df["Data"] = pd.to_datetime(df["Data"]).dt.date
 
-    columns_fmt = {"Valor": st.column_config.NumberColumn("Valor", format="R$ %.2f")}
+    columns_fmt = {
+        "Valor": st.column_config.NumberColumn("Valor", format="R$ %.2f"),
+        "Data": st.column_config.DateColumn("Data", format="DD/MM/YYYY"),
+    }
     st.dataframe(df, hide_index=True, column_config=columns_fmt)
+
+    df_instituicao = df.pivot_table(index="Data", columns="Instituição", values="Valor")
+    st.dataframe(df_instituicao, column_config=columns_fmt)
